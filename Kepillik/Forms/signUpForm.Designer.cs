62cf6f4200f36510453a8c27770028784a8ee7c6
ZDNanev19 <ZDNanev19@codingburgas.bg>
2023-05-21 14:35:44 +0300

    Filled the signUpForm with functionless objects

--- Kepillik/Forms/signUpForm.Designer.cs
+++ Kepillik/Forms/signUpForm.Designer.cs
@@ -38,7 +38,20 @@
             firstNameBox = new TextBox();
             lastNameBox = new TextBox();
             ageBox = new TextBox();
+            passBox = new TextBox();
+            repeatBox = new TextBox();
+            continueButton = new Button();
+            signUpButton = new Button();
+            UserNameBox = new TextBox();
+            openedEyeImage = new PictureBox();
+            closedEyeImage = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)openedEyeImage).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)closedEyeImage).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
@@ -149,12 +162,127 @@
             ageBox.TabIndex = 21;
             ageBox.Text = "Age";
             // 
+            // passBox
+            // 
+            passBox.BackColor = Color.FromArgb(0, 17, 48);
+            passBox.BorderStyle = BorderStyle.None;
+            passBox.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            passBox.ForeColor = Color.DimGray;
+            passBox.Location = new Point(298, 186);
+            passBox.Name = "passBox";
+            passBox.Size = new Size(188, 32);
+            passBox.TabIndex = 22;
+            passBox.Text = "     Password";
+            passBox.Visible = false;
+            // 
+            // repeatBox
+            // 
+            repeatBox.BackColor = Color.FromArgb(0, 17, 48);
+            repeatBox.BorderStyle = BorderStyle.None;
+            repeatBox.Font = new Font("Times New Roman", 18.25F, FontStyle.Bold, GraphicsUnit.Point);
+            repeatBox.ForeColor = Color.DimGray;
+            repeatBox.Location = new Point(298, 224);
+            repeatBox.Name = "repeatBox";
+            repeatBox.Size = new Size(188, 28);
+            repeatBox.TabIndex = 23;
+            repeatBox.Text = "Repeat Password";
+            repeatBox.Visible = false;
+            // 
+            // continueButton
+            // 
+            continueButton.BackColor = Color.FromArgb(0, 27, 48);
+            continueButton.FlatAppearance.BorderSize = 0;
+            continueButton.FlatStyle = FlatStyle.Flat;
+            continueButton.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            continueButton.ForeColor = Color.FromArgb(62, 230, 175);
+            continueButton.Location = new Point(459, 239);
+            continueButton.Name = "continueButton";
+            continueButton.Size = new Size(164, 43);
+            continueButton.TabIndex = 24;
+            continueButton.Text = "Continue";
+            continueButton.UseVisualStyleBackColor = false;
+            // 
+            // signUpButton
+            // 
+            signUpButton.BackColor = Color.FromArgb(0, 27, 48);
+            signUpButton.FlatAppearance.BorderSize = 0;
+            signUpButton.FlatStyle = FlatStyle.Flat;
+            signUpButton.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            signUpButton.ForeColor = Color.FromArgb(62, 230, 175);
+            signUpButton.Location = new Point(312, 288);
+            signUpButton.Name = "signUpButton";
+            signUpButton.Size = new Size(164, 43);
+            signUpButton.TabIndex = 25;
+            signUpButton.Text = "Sign up";
+            signUpButton.UseVisualStyleBackColor = false;
+            signUpButton.Visible = false;
+            // 
+            // UserNameBox
+            // 
+            UserNameBox.BackColor = Color.FromArgb(0, 17, 48);
+            UserNameBox.BorderStyle = BorderStyle.None;
+            UserNameBox.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            UserNameBox.ForeColor = Color.DimGray;
+            UserNameBox.Location = new Point(298, 139);
+            UserNameBox.Name = "UserNameBox";
+            UserNameBox.Size = new Size(188, 32);
+            UserNameBox.TabIndex = 26;
+            UserNameBox.Text = "     Username";
+            UserNameBox.Visible = false;
+            // 
+            // openedEyeImage
+            // 
+            openedEyeImage.BackgroundImage = (Image)resources.GetObject("openedEyeImage.BackgroundImage");
+            openedEyeImage.Location = new Point(508, 186);
+            openedEyeImage.Name = "openedEyeImage";
+            openedEyeImage.Size = new Size(32, 32);
+            openedEyeImage.TabIndex = 27;
+            openedEyeImage.TabStop = false;
+            openedEyeImage.Visible = false;
+            // 
+            // closedEyeImage
+            // 
+            closedEyeImage.BackgroundImage = Properties.Resources.closedeye;
+            closedEyeImage.Location = new Point(591, 186);
+            closedEyeImage.Name = "closedEyeImage";
+            closedEyeImage.Size = new Size(32, 32);
+            closedEyeImage.TabIndex = 28;
+            closedEyeImage.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = Properties.Resources.closedeye;
+            pictureBox2.Location = new Point(591, 224);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(32, 32);
+            pictureBox2.TabIndex = 29;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.Location = new Point(508, 224);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(32, 32);
+            pictureBox3.TabIndex = 30;
+            pictureBox3.TabStop = false;
+            pictureBox3.Visible = false;
+            // 
             // signUpForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(0, 11, 31);
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(closedEyeImage);
+            Controls.Add(openedEyeImage);
+            Controls.Add(UserNameBox);
+            Controls.Add(signUpButton);
+            Controls.Add(continueButton);
+            Controls.Add(repeatBox);
+            Controls.Add(passBox);
             Controls.Add(ageBox);
             Controls.Add(lastNameBox);
             Controls.Add(firstNameBox);
@@ -170,6 +298,10 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "signUpForm";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)openedEyeImage).EndInit();
+            ((System.ComponentModel.ISupportInitialize)closedEyeImage).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -185,5 +317,14 @@
         private TextBox firstNameBox;
         private TextBox lastNameBox;
         private TextBox ageBox;
+        private TextBox passBox;
+        private TextBox repeatBox;
+        private Button continueButton;
+        private Button signUpButton;
+        private TextBox UserNameBox;
+        private PictureBox openedEyeImage;
+        private PictureBox closedEyeImage;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
     }
 }
